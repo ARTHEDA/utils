49353dc2995c4d496b4daa877deeb268dbe7bd46
ARTHEDA <arth3da@gmail.com>
2025-10-17 16:19:59 +0300

    [Fix] Fix custom form field

--- lib/widgets/input/custom_form_field.dart
+++ lib/widgets/input/custom_form_field.dart
@@ -2,44 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:get/get_utils/src/extensions/context_extensions.dart';
 import 'package:get/get_utils/src/extensions/widget_extensions.dart';
 
-class CustomFormField<T> extends StatelessWidget {
-  const CustomFormField({
+class CustomFormField<T> extends FormField<T> {
+  CustomFormField({
     super.key,
-    this.formFieldKey,
-    required this.value,
-    this.validator,
-    required this.child,
-  });
-
-  final GlobalKey<FormFieldState>? formFieldKey;
-  final T value;
-  final FormFieldValidator<T>? validator;
-  final Widget child;
+    required T value,
+    super.validator,
+    required Widget child,
+  }) : super(
+          initialValue: value,
+          builder: (state) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                child,
+                if (state.errorText != null)
+                  Builder(
+                    builder: (context) {
+                      return Text(
+                        state.errorText!,
+                        style: context.theme.inputDecorationTheme.errorStyle,
+                      ).paddingOnly(
+                        left: context.theme.inputDecorationTheme.contentPadding!
+                            .resolve(Directionality.of(context))
+                            .left,
+                        top: 8,
+                      );
+                    },
+                  ),
+              ],
+            );
+          },
+        );
 
   @override
-  Widget build(BuildContext context) {
-    return FormField(
-      key: formFieldKey,
-      initialValue: value,
-      builder: (state) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            child,
-            if (state.errorText != null)
-              Text(
-                state.errorText!,
-                style: context.theme.inputDecorationTheme.errorStyle,
-              ).paddingOnly(
-                left: context.theme.inputDecorationTheme.contentPadding!
-                    .resolve(Directionality.of(context))
-                    .left,
-                top: 8,
-              ),
-          ],
-        );
-      },
-      validator: validator,
-    );
+  FormFieldState<T> createState() => _CustomFormFieldState<T>();
+}
+
+class _CustomFormFieldState<T> extends FormFieldState<T> {
+  @override
+  void didUpdateWidget(CustomFormField<T> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.initialValue != widget.initialValue) {
+      setValue(widget.initialValue);
+    }
   }
 }
